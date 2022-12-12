65f2aa4bf6260fa29c02b4547869dd9a9a3134b5
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2022-12-12 19:52:03 +0300

    tasks 1-2

---
+++ task3.java
@@ -0,0 +1,35 @@
+//Вывести все простые числа от 1 до 1000 
+//(простые числа - это числа которые делятся только на себя и на единицу без остатка. 
+//Чтобы найти остаток от 
+//деления используйте оператор % , например 10%3 будет равно единице)
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите положительное число: ");
+        int input = scanner.nextInt();
+        scanner.close();
+        List<Integer> primes = new ArrayList<>();
+
+        for (int i = 2; i <= input; i++) {
+            boolean isPrimeNumber = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+
+            if (isPrimeNumber) {
+                primes.add(i);
+            }
+        }
+        System.out.println("Простые числа: " + primes);
+    }
+}
