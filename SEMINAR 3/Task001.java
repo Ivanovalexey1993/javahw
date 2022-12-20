7f084a03474a9844818c72283ff1e2c16bbc79f7
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2022-12-20 19:20:17 +0300

    Seminar 3

--- SEMINAR 3/Task001.java
+++ SEMINAR 3/Task001.java
@@ -1,28 +1,26 @@
-//1. Пусть дан произвольный список целых чисел, удалить из него четные числа
-
-package ru.geekbrains;
-
-import java.util.ArrayList;
-import java.util.Random;
+package SEMINAR 3;
 
 public class Task001 {
+    import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1 {
+
+    // Дан список с десятью случайными числами, нужно отсортировать
     public static void main(String[] args) {
-        Random rnd = new Random();
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        for (int i = 0; i < 10; i++) {
-            int val = rnd.nextInt(-100, 100);
-            list1.add(val);
-        }
-        System.out.printf("Первоначальный список %s\n", list1);
-        int i = 0;
-        while (i < list1.size()) {
-            if (list1.get(i) % 2 == 0) {
-                list1.remove(i);
-            }
-            else {
-                i++;
-            }
-        }
-        System.out.printf("Список с удаленными четными числами %s", list1);
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 9, 2, 6, 4, 3, 5, 7, 8, 0));
+
     }
+
+    public List<Integer> sortByCollections(List<Integer> list){
+
+        return new ArrayList<>();
+    }
+
+    public List<Integer> sortByComparator(List<Integer> list){
+
+        return new ArrayList<>();
+    }
+
 }
